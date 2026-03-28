0dd36672591f04e721442eddd6def48aac50c436
samado-laouar <abdessamed.laouar2003@gmail.com>
2026-03-28 15:03:38 +0100

    Initil Setup

---
+++ src/app/(store)/page.tsx
@@ -0,0 +1,9 @@
+export default function StorePage() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold">
+        BINGO STORE Products
+      </h1>
+    </div>
+  );
+}
